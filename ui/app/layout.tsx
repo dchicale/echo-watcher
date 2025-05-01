27c848bca68c278c3e1aae55c2f061b93e0ab8b7
Danilo Chicale <danilo.chicale@live.com>
2025-05-01 20:51:43 -0300

    chore: navigation

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -1,30 +1,22 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import Footer from "@/components/footer"
-import Header from "@/components/header"
 import { ThemeProvider } from "@/components/theme-provider"
-
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "NextAuth.js Example",
+  title: "echo-watcher",
   description:
-    "This is an example site to demonstrate how to use NextAuth.js for authentication",
+    "coming soon...",
 }
 
-export default function RootLayout({ children }: React.PropsWithChildren) {
+export default async function RootLayout({ children }: React.PropsWithChildren) {
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          <div className="flex h-full min-h-screen w-full flex-col justify-between">
-            <Header />
-            <main className="mx-auto w-full max-w-3xl flex-auto px-4 py-4 sm:px-6 md:py-6">
-              {children}
-            </main>
-            <Footer />
-          </div>
+        <ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
+          {children}
         </ThemeProvider>
       </body>
     </html>
